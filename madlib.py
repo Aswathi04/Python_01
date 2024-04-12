fe9fa3fbb9c282e765a82796b2c3cb4f1d06754e
Aswathi04 <150355256+Aswathi04@users.noreply.github.com>
2024-04-13 01:54:35 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,9 @@
+#MADLIBS#
+print("Lets create something")
+print("Give appropriate inputs")
+adj=input("Adjective: ")
+verb=input("Verb: ")
+name=input("Name: ")
+no_=input("Year: ")
+madlib=f"On 27th March {no_}, {name} was born. {name} had {adj} {verb} skills and quickly became a crowd favourite."
+print(madlib)
